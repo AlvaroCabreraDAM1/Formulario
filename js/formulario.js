6dbda9cb70b8fd6ccd7ba10bce008e0ba079df5f
Alvaro Cabrera Fernandez <32370797+AlvaroCabreraDAM1@users.noreply.github.com>
2018-03-05 13:55:17 +0100

    Add files via upload

--- js/formulario.js
+++ js/formulario.js
@@ -1,71 +1,112 @@
-// fichoro xml que está en el servidor rawgit
-var url="https://rawgit.com/AlvaroCabreraDAM1/Formulario/master/xml/formulario.xml";
+// Fichoro .xml que está en rawgit
+
+var url = "https://rawgit.com/AlvaroCabreraDAM1/Formulario/master/xml/formulario.xml";
 
 var xhttp = new XMLHttpRequest();
 
+var a = 0;
+var b = 0;
+var c = 0;
+var r = 0;
+
 xhttp.onreadystatechange = function() {
+	
 	if (this.readyState == 4 && this.status == 200) {
 		// función personalizada que gestiona la respuesta a la petición de fichero
 		gestionarXml(this); 
 	}
-};
+	
+}
 
 xhttp.open("GET", url, true); //url del fichero
 xhttp.send();
 
 // función personalizada que gestiona la respuesta a la petición de fichero
+
 function gestionarXml(dadesXml){
+	
 	var xmlDoc = dadesXml.responseXML;
-	//Pregunta 1
-	document.getElementById("pregunta1").innerHTML = xmlDoc.getElementsByTagName("title")[0].childNodes[0].nodeValue;
-	//Pregunta 2
-	document.getElementById("pregunta2").innerHTML = xmlDoc.getElementsByTagName("title")[1].childNodes[0].nodeValue;
-	//Pregunta 3
-    document.getElementById("pregunta3").innerHTML = xmlDoc.getElementsByTagName("title")[2].childNodes[0].nodeValue;
-	document.getElementsByTagName("option")[0].innerHTML = xmlDoc.getElementsByTagName("question")[2].getElementsByTagName("option")[0].childNodes[0].nodeValue;
-	document.getElementsByTagName("option")[1].innerHTML = xmlDoc.getElementsByTagName("question")[2].getElementsByTagName("option")[1].childNodes[0].nodeValue;		
-	document.getElementsByTagName("option")[2].innerHTML = xmlDoc.getElementsByTagName("question")[2].getElementsByTagName("option")[2].childNodes[0].nodeValue;
-	document.getElementsByTagName("option")[3].innerHTML = xmlDoc.getElementsByTagName("question")[2].getElementsByTagName("option")[3].childNodes[0].nodeValue;
-	//Pregunta 4
-	document.getElementById("pregunta4").innerHTML = xmlDoc.getElementsByTagName("title")[3].childNodes[0].nodeValue;
-	document.getElementsByTagName("option")[4].innerHTML = xmlDoc.getElementsByTagName("question")[3].getElementsByTagName("option")[0].childNodes[0].nodeValue;
-	document.getElementsByTagName("option")[5].innerHTML = xmlDoc.getElementsByTagName("question")[3].getElementsByTagName("option")[1].childNodes[0].nodeValue;		
-	document.getElementsByTagName("option")[6].innerHTML = xmlDoc.getElementsByTagName("question")[3].getElementsByTagName("option")[2].childNodes[0].nodeValue;
-	document.getElementsByTagName("option")[7].innerHTML = xmlDoc.getElementsByTagName("question")[3].getElementsByTagName("option")[3].childNodes[0].nodeValue;
-	//Pregunta 5
-	document.getElementById("pregunta5").innerHTML = xmlDoc.getElementsByTagName("title")[4].childNodes[0].nodeValue;
-	document.getElementsByTagName("option")[8].innerHTML = xmlDoc.getElementsByTagName("question")[4].getElementsByTagName("option")[0].childNodes[0].nodeValue;
-	document.getElementsByTagName("option")[9].innerHTML = xmlDoc.getElementsByTagName("question")[4].getElementsByTagName("option")[1].childNodes[0].nodeValue;		
-	document.getElementsByTagName("option")[10].innerHTML = xmlDoc.getElementsByTagName("question")[4].getElementsByTagName("option")[2].childNodes[0].nodeValue;
-	document.getElementsByTagName("option")[11].innerHTML = xmlDoc.getElementsByTagName("question")[4].getElementsByTagName("option")[3].childNodes[0].nodeValue;
-	//Pregunta 6
-	document.getElementById("pregunta6").innerHTML = xmlDoc.getElementsByTagName("title")[5].childNodes[0].nodeValue;
-	document.getElementsByTagName("option")[12].innerHTML = xmlDoc.getElementsByTagName("question")[5].getElementsByTagName("option")[0].childNodes[0].nodeValue;
-	document.getElementsByTagName("option")[13].innerHTML = xmlDoc.getElementsByTagName("question")[5].getElementsByTagName("option")[1].childNodes[0].nodeValue;		
-	document.getElementsByTagName("option")[14].innerHTML = xmlDoc.getElementsByTagName("question")[5].getElementsByTagName("option")[2].childNodes[0].nodeValue;
-	document.getElementsByTagName("option")[15].innerHTML = xmlDoc.getElementsByTagName("question")[5].getElementsByTagName("option")[3].childNodes[0].nodeValue;
-	//Pregunta 7
-	document.getElementById("pregunta7").innerHTML = xmlDoc.getElementsByTagName("title")[6].childNodes[0].nodeValue;
-	document.getElementsByTagName("label")[0].innerHTML = xmlDoc.getElementsByTagName("question")[6].getElementsByTagName("option")[0].childNodes[0].nodeValue;
-	document.getElementsByTagName("label")[1].innerHTML = xmlDoc.getElementsByTagName("question")[6].getElementsByTagName("option")[1].childNodes[0].nodeValue;		
-	document.getElementsByTagName("label")[2].innerHTML = xmlDoc.getElementsByTagName("question")[6].getElementsByTagName("option")[2].childNodes[0].nodeValue;
-	document.getElementsByTagName("label")[3].innerHTML = xmlDoc.getElementsByTagName("question")[6].getElementsByTagName("option")[3].childNodes[0].nodeValue;
-	//Pregunta 8
-	document.getElementById("pregunta8").innerHTML = xmlDoc.getElementsByTagName("title")[7].childNodes[0].nodeValue;
-	document.getElementsByTagName("label")[4].innerHTML = xmlDoc.getElementsByTagName("question")[7].getElementsByTagName("option")[0].childNodes[0].nodeValue;
-	document.getElementsByTagName("label")[5].innerHTML = xmlDoc.getElementsByTagName("question")[7].getElementsByTagName("option")[1].childNodes[0].nodeValue;		
-	document.getElementsByTagName("label")[6].innerHTML = xmlDoc.getElementsByTagName("question")[7].getElementsByTagName("option")[2].childNodes[0].nodeValue;
-	document.getElementsByTagName("label")[7].innerHTML = xmlDoc.getElementsByTagName("question")[7].getElementsByTagName("option")[3].childNodes[0].nodeValue;
-	//Pregunta 9
-	document.getElementById("pregunta9").innerHTML = xmlDoc.getElementsByTagName("title")[8].childNodes[0].nodeValue;
-	document.getElementsByTagName("label")[8].innerHTML = xmlDoc.getElementsByTagName("question")[8].getElementsByTagName("option")[0].childNodes[0].nodeValue;
-	document.getElementsByTagName("label")[9].innerHTML = xmlDoc.getElementsByTagName("question")[8].getElementsByTagName("option")[1].childNodes[0].nodeValue;		
-	document.getElementsByTagName("label")[10].innerHTML = xmlDoc.getElementsByTagName("question")[8].getElementsByTagName("option")[2].childNodes[0].nodeValue;
-	document.getElementsByTagName("label")[11].innerHTML = xmlDoc.getElementsByTagName("question")[8].getElementsByTagName("option")[3].childNodes[0].nodeValue;
-	//Pregunta 10
-	document.getElementById("pregunta10").innerHTML = xmlDoc.getElementsByTagName("title")[9].childNodes[0].nodeValue;
-	document.getElementsByTagName("label")[12].innerHTML = xmlDoc.getElementsByTagName("question")[9].getElementsByTagName("option")[0].childNodes[0].nodeValue;
-	document.getElementsByTagName("label")[13].innerHTML = xmlDoc.getElementsByTagName("question")[9].getElementsByTagName("option")[1].childNodes[0].nodeValue;		
-	document.getElementsByTagName("label")[14].innerHTML = xmlDoc.getElementsByTagName("question")[9].getElementsByTagName("option")[2].childNodes[0].nodeValue;
-	document.getElementsByTagName("label")[15].innerHTML = xmlDoc.getElementsByTagName("question")[9].getElementsByTagName("option")[3].childNodes[0].nodeValue;
+	
+	//Titulos
+	
+	for (var aux = 0; aux < xmlDoc.getElementsByTagName("title").length; aux ++) {
+		document.getElementsByClassName("preguntaT")[aux].innerHTML = xmlDoc.getElementsByTagName("title")[aux].childNodes[0].nodeValue;
 	}
+	
+	//Preguntas Select
+	
+	for (numPregunta=2; numPregunta< 6; numPregunta++) {
+        var opcionesSelect = [];
+        var nopt = xmlDoc.getElementsByTagName("question")[numPregunta].getElementsByTagName('option').length;
+        for (i = 0; i < nopt; i++) {
+            opcionesSelect[i] = xmlDoc.getElementsByTagName("question")[numPregunta].getElementsByTagName('option')[i].innerHTML;
+        }
+        ponerDatosSelectHtml( opcionesSelect, numPregunta);
+    }
+
+	//Preguntas Chaeckbox
+	
+	for (numPregunta=6; numPregunta<8; numPregunta++) {
+        var opcionesSelect = [];
+        var nopt = xmlDoc.getElementsByTagName("question")[numPregunta].getElementsByTagName('option').length;
+        for (i = 0; i < nopt; i++) {
+            opcionesSelect[i] = xmlDoc.getElementsByTagName("question")[numPregunta].getElementsByTagName('option')[i].innerHTML;
+        }
+        ponerDatosCheckboxHtml( opcionesSelect, numPregunta);
+    }
+	
+	//Preguntas Radio
+	
+	for (numPregunta=8; numPregunta<10; numPregunta++) {
+        var opcionesSelect = [];
+        var nopt = xmlDoc.getElementsByTagName("question")[numPregunta].getElementsByTagName('option').length;
+        for (i = 0; i < nopt; i++) {
+            opcionesSelect[i] = xmlDoc.getElementsByTagName("question")[numPregunta].getElementsByTagName('option')[i].innerHTML;
+        }
+        ponerDatosRadioHtml( opcionesSelect, numPregunta);
+    }
+}
+
+function ponerDatosSelectHtml( opt, numPregunta) {
+	var select = document.getElementsByTagName("select")[numPregunta-2];
+	for (i = 0; i < opt.length; i++) {
+		var option = document.createElement("option");
+		option.text = opt[i];
+		option.value = i;
+		select.options.add(option);
+	}
+}
+	
+function ponerDatosCheckboxHtml( opt, numPregunta) {
+	var radioCont = document.getElementsByClassName("preguntaC")[numPregunta-6];
+	for (i = 0; i < opt.length; i++,c ++) {
+		var input = document.createElement("input");
+		var label = document.createElement("label");
+		label.innerHTML=opt[i];
+		label.htmlFor=("checkbox" + c);
+		input.id=("checkbox" + c);
+		input.type="checkbox";
+		input.value=i;
+		radioCont.appendChild(input);
+		radioCont.appendChild(label);
+		radioCont.appendChild(document.createElement("br"));
+	}
+}
+
+function ponerDatosRadioHtml( opt, numPregunta) {
+	var radioCont = document.getElementsByClassName("preguntaR")[numPregunta-8];
+	for (i = 0; i < opt.length; i++, r++) {
+		var input = document.createElement("input");
+		var label = document.createElement("label");
+		label.innerHTML=opt[i];
+		label.htmlFor=("radio" + r);
+		input.id=("radio" + r);
+		input.type="radio";
+		input.value=i;
+		input.name=("nombre"+a);
+		radioCont.appendChild(input);
+		radioCont.appendChild(label);
+		radioCont.appendChild(document.createElement("br"));
+		}
+	a++;
+}
